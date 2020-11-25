6b5fdcc943f7e51736389e8074fb38e67a62cf76
Adam Coyle <acoyle22@qub.ac.uk>
2020-11-25 10:25:24 +0000

    add helper functions from mpi assignment.

--- src/HPCProject/project_MPI.c
+++ src/HPCProject/project_MPI.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <time.h>
 #include <dirent.h>
+#include <mpi.h>
 
 #define MAX_TEXTS 20
 #define MAX_PATTERNS 20 // based on assumptions from assignment brief
@@ -12,6 +13,8 @@
 
 #define BYTES_PER_LINE 20 // 4 bytes per character * 5 characters
 #define BUFFER_SIZE 2000
+
+#define MASTER 0
 
 char *textData[MAX_TEXTS];
 int textLengths[MAX_TEXTS];
@@ -153,4 +156,99 @@ void writeToBuffer(char buffer[], int textNumber, int patternNumber, int pattern
     }
     // append new result to buffer
     sprintf(buffer + strlen(buffer), "%i %i %i\n", textNumber, patternNumber, patternLocation);
+}
+
+/// <summary>
+/// Distributes text length among processes.
+/// </summary>
+/// <param name="procWork">Array to contain the workload (length of allocated text) of each process.</param>
+/// <param name="nProc">Number of processes in the program.</param>
+/// <param name="textLength">The length of the full text.</param>
+void divideWorkload(int* procWork, int nProc, int textLength)
+{
+    // calculate the base number of elements for each process
+    int nElements = textLength / nProc;
+    //printf("\nnElements = %i\n", nElements); // confirm base elements 
+
+    // set base number of elements for each process i
+    int i;
+    for (i = 0; i < nProc; i++)
+    {
+        (*(procWork + i)) = nElements;
+    }
+
+    int remainder = textLength % nProc;
+    //printf("\nRemainder = %i\n", remainder); // confirm remainder
+
+    // if there are no remainders, we can continue with the program
+    if (remainder == 0)
+    {
+        return;
+    }
+
+    // assign remainders to slave processes
+    // we work backwards through processes such that only slave processes
+    // take on any extra workload
+    for (i = (nProc - 1); i > ((nProc - 1) - remainder); i--)
+    {
+        (*(procWork + i)) += 1;
+    }
+}
+
+/// <summary>
+/// Sets the displacement in the full text for each process.
+/// </summary>
+/// <param name="displs">Array to contain the displacement of each process.</param>
+/// <param name="procWork">Array containing the workload of each process.</param>
+/// <param name="nProc">Number of processes in the program.</param>
+void setDisplacement(int* displs, int* procWork, int nProc)
+{
+    int i;
+    // displacement at i dependent on i-1. We can set displs[0] to 0 since we know it starts there
+    displs[0] = 0;
+    for (i = 1; i < nProc; i++)
+    {
+        displs[i] = displs[i - 1] + procWork[i - 1];
+        //printf("Process %i displacement = %i\n", i, displs[i]);
+    }
+}
+
+
+void processMaster(char* directory)
+{
+
+}
+
+void processSlave()
+{
+
+}
+
+void main(int argc, char** argv)
+{
+
+
+    int nProc, procId;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nProc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &procId);
+
+    // exit if no input directory specified, or fewer than 4 cores
+    if (argc < 1 || nProc < 4)
+    {
+        printf("Not enough arguments: No inputs directory provided.");
+        exit(0);
+    }
+
+    if (procId == MASTER)
+    {
+        processMaster(argv[1]);
+    }
+    else
+    {
+        processSlave();
+    }
+
+    MPI_Finalize();
 }
